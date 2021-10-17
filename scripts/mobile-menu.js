255be0a2d0484290adb78d0596985bd1133e683d
DavidaaWoW <davidik.gegiya@gmail.com>
2021-10-17 20:25:02 +0400

    first commit

---
+++ scripts/mobile-menu.js
@@ -0,0 +1,9 @@
+let menu = document.getElementsByClassName("menu-hideable");
+
+function mobilemenuShow() {
+  menu[0].style.display = "flex";
+}
+
+function mobilemenuHide() {
+  menu[0].style.display = "none";
+}
